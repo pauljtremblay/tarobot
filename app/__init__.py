6857c9d067ae1215b8ac1603a2a689b9a58d9e6c
Paul Tremblay <35076933+pauljtremblay@users.noreply.github.com>
2023-03-26 14:42:31 -0400

    Move application logic into own module, class. Start parsing command line arguments for parameters.

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Tarot deck cartomancy application.
+
+This module processes command line arguments and executes the desired command.
+"""
+
+__all__ = ['App']
+
+from app.app import App
---
+++ app/app.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+from dotenv import load_dotenv
+import openai
+import os
+from tarot import TarotDeck
+
+APP_NAME = "Tarobot"
+API_KEY_ENV_VAR = "OPENAI_API_KEY"
+
+
+class MissingEnvVar(Exception):
+    """This exception is raised when a required environment variable has not been defined."""
+    pass
+
+
+class App:
+    """This class processes input from command line arguments and executes the request."""
+
+    def __init__(self):
+        # initialize openai module, or error out if api key is not defined
+        load_dotenv()
+        try:
+            openai.api_key = os.environ[API_KEY_ENV_VAR]
+        except KeyError:
+            raise MissingEnvVar("%s requires an api key set in env var %s" % (APP_NAME, API_KEY_ENV_VAR))
+        self.__model = "text-davinci-003"
+        self.__subject = None
+        self.__teller = None
+        self.deck = None
+        self.card_count = None
+        self.spread = None
+
+    def main(self):
+        """The main method: draws 3 tarot cards at random and has openai generate a tarot card reading."""
+        self.parse_command_line_args()
+        self.draw_tarot_spread()
+        self.interpret_tarot_spread()
+
+    def draw_tarot_spread(self):
+        """Shuffles a fresh tarot deck and draws N cards."""
+        self.deck = TarotDeck()
+        self.spread = self.deck.draw(self.card_count)
+
+    def interpret_tarot_spread(self):
+        """Generates a tarot card reading for the given spread and prints to standard output."""
+        print("Generating a tarot card reading for {} for the following spread: {}\n".format(
+            self.__subject, str(self.spread).strip("[]")))
+        tarot_reading_prompt = self.__generate_tarot_reading_prompt()
+        _, response = self.__ask_openai_to_generate_response(tarot_reading_prompt)
+        print("Response:\n{}".format(response))
+
+    def parse_command_line_args(self):
+        """Parses command line arguments into instructions for the application."""
+        parser = ArgumentParser(prog='tarobot',
+                                description='Tarot deck cartomancy application')
+        parser.add_argument('--card_count',
+                            help='number of tarot cards to draw in the spread [1-5]',
+                            type=int,
+                            choices=range(1, 5 + 1),
+                            default=3)
+        parser.add_argument('--subject',
+                            help='the name of the person receiving the tarot card reading',
+                            type=str,
+                            default='the seeker')
+        parser.add_argument('--teller',
+                            help='the "person" conducting the tarot card reading',
+                            type=str,
+                            choices=[
+                                'an optimist',
+                                'a cynical old man'
+                            ])
+        args = parser.parse_args()
+        self.card_count = args.card_count
+        self.__subject = args.subject
+        if args.teller is not None:
+            self.__teller = args.teller
+
+    def __generate_tarot_reading_prompt(self):
+        """Generates the prompt to openai for how to generate a tarot card reading for the given spread."""
+        prompt = "Tarot card reading for {} with the cards ".format(self.__subject)
+        if len(self.spread) < 2:
+            prompt += self.spread[0]
+        else:
+            [*head, tail] = self.spread
+            prompt += ", ".join(str(card) for card in head) + ", and " + str(tail)
+        if self.__teller is not None:
+            prompt += " in the style of " + self.__teller
+        return prompt
+
+    def __ask_openai_to_generate_response(self, prompt):
+        """Displays the prompt to and associated response from openai."""
+        completion = openai.Completion.create(model=self.__model, prompt=prompt, max_tokens=2000)
+        response = "\n".join(list(choice.text for choice in completion.choices)).strip()
+        return completion, response
