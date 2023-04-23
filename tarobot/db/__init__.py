1dc703cd919b526502c95a9e586bd675856fa5a6
Paul Tremblay <35076933+pauljtremblay@users.noreply.github.com>
2023-04-23 10:13:37 -0400

    Attempting to simplify imports

--- tarobot/db/__init__.py
+++ tarobot/db/__init__.py
@@ -5,7 +5,7 @@
 This module manages storing application data in a relational database.
 """
 
-__all__ = ['session_factory', 'Base', 'CardReadingEntity']
+__all__ = ['session_factory', 'CardReadingEntity']
 
-from . base import session_factory, Base
+from . base import session_factory
 from . card_reading_entity import CardReadingEntity
